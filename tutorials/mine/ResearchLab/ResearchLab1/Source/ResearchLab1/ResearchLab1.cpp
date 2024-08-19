a3a5be9c22327623bf906405489a570b2e271355
LordSahvith <bulletproofchild@gmail.com>
2024-08-19 16:48:51 -0600

    init

---
+++ tutorials/mine/ResearchLab/ResearchLab1/Source/ResearchLab1/ResearchLab1.cpp
@@ -0,0 +1,7 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "ResearchLab1.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, ResearchLab1, "ResearchLab1" );
+ 
---
+++ tutorials/mine/ResearchLab/ResearchLab1/Source/ResearchLab1/ResearchLab1GameMode.h
@@ -0,0 +1,19 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "ResearchLab1GameMode.generated.h"
+
+UCLASS(minimalapi)
+class AResearchLab1GameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	AResearchLab1GameMode();
+};
+
+
+
